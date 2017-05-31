8a1a8ef9a7dfa3ded6e0482e6cbe19c51e5a0cf5
Felipe Cecagno <fcecagno@gmail.com>
2017-05-31 10:48:02 -0300

    new info on metadata

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,5 @@ license          'MPLv2'
 description      'Installs/Configures libopenh264'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-recipe "openh264::default"
-recipe "openh264::source"
+recipe "openh264::default", "Wrapper for the source recipe"
+recipe "openh264::source", "Install openh264 by source"
