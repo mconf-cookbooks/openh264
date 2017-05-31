ca3d858440ee55b782334d4d0cd0f4eb34119145
Felipe Cecagno <fcecagno@gmail.com>
2017-05-31 10:47:05 -0300

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'MPLv2'
 description      'Installs/Configures libopenh264'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+recipe "openh264::default"
+recipe "openh264::source"
